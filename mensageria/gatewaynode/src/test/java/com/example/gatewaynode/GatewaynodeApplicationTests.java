f53af9aa010893d3771789f987d9fd6ea3392940
anacarolcortez <anacarolcortez@hotmail.com>
2022-12-02 08:52:03 -0300

    gateway e mscartoes

---
+++ mensageria/gatewaynode/src/test/java/com/example/gatewaynode/GatewaynodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaynode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaynodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
